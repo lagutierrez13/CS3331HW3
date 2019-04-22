c010877b42d9d0c97c22b6107e9fd9ca453dbd5a
amacielcue <43260850+amacielcue@users.noreply.github.com>
2019-04-22 03:23:53 -0600

    Add files via upload

---
+++ HW3/src/hw3test1/ItemViewManager.java
@@ -0,0 +1,36 @@
+/*
+ * CS 3331 - Dr. Cheon
+ * Created by: Alejandra Maciel, Luis Gutierrez & Victor Huicochea
+ * Last Edit Date: 04/21/2019
+ */
+
+package hw3test1;
+
+import base.ItemView;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+
+public class ItemViewManager {
+	DefaultListModel<ItemView> itemViews;
+
+	public ItemViewManager(DefaultListModel<ItemView> itemViews) {
+		this.itemViews = itemViews;
+	}
+
+	public ItemViewManager() {
+		this.itemViews = new DefaultListModel<ItemView>();
+	}
+
+	public void addItem(ItemView item) {
+		this.itemViews.addElement(item);
+	}
+
+	public void removeItem(int index) {
+		this.itemViews.removeElementAt(index);
+	}
+
+	public DefaultListModel<ItemView> getItemViews() {
+		return this.itemViews;
+	}
+}
